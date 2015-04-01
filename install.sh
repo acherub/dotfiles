f5d5ff2b6b188bc73b77e352b4cd95949ceeb69f
Ivan Chi <ivan.chi@garmin.com>
2015-04-01 03:26:01 -0700

    Inital commit of .tmux.conf

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 function doIt() {
     # Copy the files
-    for file in .{gitconfig,gitattributes,gitignore,screenrc,bash_profile,extra,aliases,bash_prompt,functions,exports}; do
+    for file in .{gitconfig,gitattributes,gitignore,screenrc,bash_profile,extra,aliases,bash_prompt,functions,exports,vimrc,tmux.conf}; do
 	[ -r "$file" ] && [ -f "$file" ] && cp $(pwd)/$file ~/$file && echo copy $file;
     done;
     source ~/.bash_profile;
